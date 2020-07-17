0d4cf2f8d2f74ae6e076eb6a7b07033e6774c944
jayjain2 <jjain654@gmail.com>
2020-07-17 15:43:40 -0500

    Some minor changes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <h3> Hello Team!!!!!!</h3>
+    <h3> Hello Teams!!!!!!</h3>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     
